073f0d49b5c2e78cb0113ad4654bf30ad46e065a
RAVI <124796706+Rajasthanichora@users.noreply.github.com>
2023-04-18 19:48:22 +0530

    slider javascript complete

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,3 +1,5 @@
+// ! SLIDER START  //////////////////
+// ? category card slider start
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 const carousel = document.querySelector(".carousel-container");
@@ -26,3 +28,39 @@ prev.addEventListener("click", function () {
   track.style.transform = "translateX(" + index * -width + "px)";
   track.style.transition = "0.5s";
 });
+// ? category card slider end
+// ////////////////////////////
+// ? banner (top-header) slider start
+
+const prevSlide = document.querySelector(".prev-slide");
+const nextSlide = document.querySelector(".next-slide");
+const bannerSlider = document.querySelector(".slider");
+const sliderTrack = document.querySelector("#slidercontainer");
+let widthh = bannerSlider.offsetWidth;
+let indexx = 0;
+
+window.addEventListener("resize", function () {
+  widthh = bannerSlider.offsetWidth;
+});
+nextSlide.addEventListener("click", function (e) {
+  e.preventDefault();
+  indexx = indexx + 1.6;
+  prevSlide.classList.add("show");
+  sliderTrack.style.transform = "translateX(" + indexx * -width + "px)";
+  sliderTrack.style.transition = "0.5s";
+  if (3000 < indexx * widthh) {
+    nextSlide.classList.add("hide");
+  }
+});
+prevSlide.addEventListener("click", function () {
+  indexx = indexx - 1.6;
+  nextSlide.classList.remove("hide");
+  if (indexx === 0) {
+    prevSlide.classList.remove("show");
+    console.log(indexx)
+  }
+  sliderTrack.style.transform = "translateX(" + indexx * -widthh + "px)";
+  sliderTrack.style.transition = "0.5s";
+});
+// ? banner (top-header) slider end
+// ! SLIDER END ////////////////////
